8fdaec4cd67c0be69354a6eba23ea1a3bfd3bef4
Sarju Mulmi <mulmi.sarju@gmail.com>
2019-11-08 11:35:38 -0500

    Updates

---
+++ precache-manifest.4b2bb739275640a638e13a8e08ec5f20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80e08fa44862c99919dec13a213dd25",
+    "url": "/interactive-paycard/index.html"
+  },
+  {
+    "revision": "ec95c6187474977fee43",
+    "url": "/interactive-paycard/static/css/main.a74ccdf4.chunk.css"
+  },
+  {
+    "revision": "4810fdf281d41c690856",
+    "url": "/interactive-paycard/static/js/2.8a1f2b6d.chunk.js"
+  },
+  {
+    "revision": "ec95c6187474977fee43",
+    "url": "/interactive-paycard/static/js/main.5a8a5d02.chunk.js"
+  },
+  {
+    "revision": "c27e654e81a7c1d239bf",
+    "url": "/interactive-paycard/static/js/runtime-main.2b79954d.js"
+  },
+  {
+    "revision": "cd0636da63f755981c343ca26a769f17",
+    "url": "/interactive-paycard/static/media/21.cd0636da.jpeg"
+  },
+  {
+    "revision": "2c37b1d33b45b2158112bb8172af579b",
+    "url": "/interactive-paycard/static/media/chip.2c37b1d3.png"
+  }
+]);
